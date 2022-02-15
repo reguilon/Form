7bad7eda2cb967aa04a2d0265f57651775ddbfc2
reguilon <myeongju.ko@sellerhub.co.kr>
2022-02-15 15:36:55 +0900

    feat. 파일럿 프로젝트 > 유효성 검사

---
+++ common.js
@@ -0,0 +1,13 @@
+//joinform_check 함수로 유효성 검사
+function joinform_check() {
+    var uid = document.getElementById("uid");
+    var pwd = document.getElementById("pwd");
+
+
+
+
+    if (uid.value == "") { //해당 입력값이 없을 경우 같은 말 : if(!uid.value)
+        
+
+    }
+}

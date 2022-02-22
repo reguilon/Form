5d54fd5e5b12a996230bc62eb8c4b07db67e6607
reguilon <myeongju.ko@sellerhub.co.kr>
2022-02-22 18:41:54 +0900

    feat. 파일럿 프로젝트 > 유효성 검사

--- common.js
+++ common.js
@@ -1,38 +1,50 @@
 window.onload = function() {
-    var uid = document.getElementById("uid");
-    var pwd = document.getElementById("pwd");
-    var repwd = document.getElementById("repwd");
-    var uname = document.getElementById("uname");
-    var year = document.getElementById("yy");
-    var day = document.getElementById("dd");
-    var idCheck = /^[a-z0-9_-]{5,20}$/; // ??? + ?? + ???/??? ?? 5~20??
-    var pwdCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/; // ?? ???? + ?? + ???? 8~16??
-    var nameCheck = /^[?-?|?-?|?-?]{2,4}/; //?? 2~4 ??
+    let uid = document.getElementById("uid");
+    let pwd = document.getElementById("pwd");
+    let repwd = document.getElementById("repwd");
+    let uname = document.getElementById("uname");
+    let year = document.getElementById("yy");
+    let day = document.getElementById("dd");
+    let umail = document.getElementById("mail");
+    let idCheck = /^[a-z0-9_-]{5,20}$/; 
+    let pwdCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/; 
+    // let nameCheck = /^[?-?]{2,4}$/;
+    let yearCheck = /^[0-9]{4}/;
+    let dayCheck = /^[0-9]{1,2}/;
+    let mailCheck = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
-    uid.onblur = function(e) { // ??? ???? ?
-        if (uid.value == "") { // ?? ?? ?? ?
+    uid.onblur = function(e) {
+        if (uid.value === "") {
             document.getElementsByClassName('essential')[0].style.display = "block";
-            uid.focus(); // id? ??
-            return false; // ???? ???? ??? ?? ???? ???? ???? ? ?
-        }
-        if (!idCheck.test(uid.value)) { // id? ??? ?? ???? ?? ?? ?
+            uid.focus(); 
+            return false;
+        };
+        if (!idCheck.test(uid.value)) {
             document.getElementsByClassName('announce')[0].style.display = "block";
             uid.focus();
             return false;
-        }
+        };
+        if (idCheck.test(uid.value)) {
+            document.getElementsByClassName('announce')[0].style.display = "none";
+            document.getElementsByClassName('essential')[0].style.display = "none";
+        };
     };
 
     pwd.onblur = function(e) {
-        if (pwd.value == "") {
+        if (pwd.value === "") {
             document.getElementsByClassName('essential')[1].style.display = "block";
             pwd.focus();
             return false;
-        }
+        };
         if (!pwdCheck.test(pwd.value)) {
             document.getElementsByClassName('announce')[1].style.display = "block";
             pwd.focus();
             return false;
-        }
+        };
+        if (pwdCheck.test(pwd.value)) {
+            document.getElementsByClassName('announce')[1].style.display = "none";
+            document.getElementsByClassName('essential')[1].style.display = "none";
+        };
     };
 
     repwd.onblur = function(e) {
@@ -40,37 +52,78 @@ window.onload = function() {
             document.getElementsByClassName('announce')[2].style.display = "block";
             repwd.focus();
             return false;
+        };
+        if (repwd.value == pwd.value) {
+            document.getElementsByClassName('announce')[2].style.display = "none";
         }
     };
     
     uname.onblur = function(e) {
-        if (uname.value == "") {
+        if (uname.value === "") {
             document.getElementsByClassName('essential')[2].style.display = "block";
             uname.focus();
             return false;
-        }
+        };
         if (!nameCheck.test(uname.value)) {
             document.getElementsByClassName('announce')[3].style.display = "block";
             uname.focus();
             return false;
-        }
+        };
+        if (nameCheck.test(uname.value)) {
+            document.getElementsByClassName('announce')[3].style.display = "none";
+            document.getElementsByClassName('essential')[2].style.display = "none";
+        };
     };
 
     year.onblur = function(e) {
-        if (year.value == "") {
-            document.getElementsByClassName('annouce')[4].style.display = "block";
+        if (year.value === "") {
+            document.getElementsByClassName('announce')[4].style.display = "block";
             year.focus();
             return false;
-        }
-    }
+        };
+        if (!yearCheck.test(year.value)) {
+            document.getElementsByClassName('announce')[4].style.display = "block";
+            year.focus();
+            return false;
+        };
+        if (yearCheck.test(year.value)) {
+            document.getElementsByClassName('announce')[4].style.display = "none";
+        };
+    };
 
     day.onblur = function(e) {
-        if (day.value == "") {
-            document.getElementsByClassName('annouce')[5].style.display = "block";
+        if (day.value === "") {
+            document.getElementsByClassName('announce')[5].style.display = "block";
             day.focus();
             return false;
-        }
-    }
-};
+        };
+        if (!dayCheck.test(day.value)) {
+            document.getElementsByClassName('announce')[5].style.display = "block";
+            day.focus();
+            return false;
+        };
+        if (dayCheck.test(day.value)) {
+            document.getElementsByClassName('announce')[5].style.display = "none";
+        };
+    };
 
-// ?? ??? ????
+    umail.onblur = function(e) {
+        if (umail.value === "") {
+            document.getElementsByClassName('essential')[3].style.display = "block";
+            umail.focus();
+            return false;
+        };
+        if (!mailCheck.test(umail.value)) {
+            document.getElementsByClassName('announce')[6].style.display = "block";
+            umail.focus();
+            return false;
+        };
+        if (mailCheck.test(umail.value)) {
+            document.getElementsByClassName('essential')[3].style.display = "none";
+            document.getElementsByClassName('announce')[6].style.display = "none";
+        };
+    };
+};
+function join_btn() {
+    alert("????? ?? ?????");
+};
